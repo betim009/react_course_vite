1ff6085f8cbbdfa956b80d40abc1649986f50026
Alberto <betim009@gmail.com>
2023-09-05 16:18:48 -0300

    feat: component birthday

--- Introduction/src/App.jsx
+++ Introduction/src/App.jsx
@@ -1,14 +1,12 @@
-import Header from './components/Header'
-import Main from './components/Main'
-
+import HelloWorld from './components/HelloWorld';
 import './App.css'
+
 
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-    </>
+    <div className="App">
+      < HelloWorld />
+    </div>
   )
 }
 
---
+++ Introduction/src/components/Details.jsx
@@ -0,0 +1,11 @@
+function Details() {
+    const modules = ['✅ fundamentos', '⏳ front-end', '⏳ back-end', '⏳ cs'];
+    return (
+        <>
+            <p>{modules[0]}</p>
+            <p>{modules[1]}</p>
+        </>
+    );
+}
+
+export default Details;
